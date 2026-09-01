b711bc4863b74c638797a6f988bbe78400070b7c
Benjamin Parkkonen <bhy880@myy.haaga-helia.fi>
2026-09-01 11:39:41 +0300

    first commit

---
+++ src/main/java/hh/bhy880/bookstore/domain/Book.java
@@ -0,0 +1,11 @@
+package hh.bhy880.bookstore.domain;
+
+public class Book {
+
+    String title;
+    String author;
+    int publicationYear;
+    long isbn;
+    double price;
+
+}
---
+++ src/test/java/hh/bhy880/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.bhy880.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
