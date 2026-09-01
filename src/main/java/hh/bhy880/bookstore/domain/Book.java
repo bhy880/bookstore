c1ad181f5311cd111e9db7d5ff002ee16ae0cb17
Benjamin Parkkonen <bhy880@myy.haaga-helia.fi>
2026-09-01 13:02:45 +0300

    getters setter 2

--- src/main/java/hh/bhy880/bookstore/domain/Book.java
+++ src/main/java/hh/bhy880/bookstore/domain/Book.java
@@ -7,5 +7,38 @@ public class Book {
     int publicationYear;
     long isbn;
     double price;
+    
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+    public long getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    
 
 }
